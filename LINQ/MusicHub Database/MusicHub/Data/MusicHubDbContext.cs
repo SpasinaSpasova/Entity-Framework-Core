037cba483f0916beff87a46eff14cba6b760145d
Spasina Spasova <75899012+SpasinaSpasova@users.noreply.github.com>
2021-11-04 22:14:35 +0200

    LINQ

--- LINQ/MusicHub Database/MusicHub/Data/MusicHubDbContext.cs
+++ LINQ/MusicHub Database/MusicHub/Data/MusicHubDbContext.cs
@@ -1,6 +1,7 @@
 ﻿namespace MusicHub.Data
 {
     using Microsoft.EntityFrameworkCore;
+    using MusicHub.Data.Models;
 
     public class MusicHubDbContext : DbContext
     {
@@ -12,6 +13,12 @@
             : base(options)
         {
         }
+        public virtual DbSet<Album> Albums { get; set; }
+        public virtual DbSet<Performer> Performers { get; set; }
+        public virtual DbSet<Producer> Producers { get; set; }
+        public virtual DbSet<Song> Songs { get; set; }
+        public virtual DbSet<SongPerformer> SongsPerformers { get; set; }
+        public virtual DbSet<Writer> Writers { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
@@ -24,7 +31,10 @@
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
-            
+            builder.Entity<SongPerformer>(e =>
+            {
+                e.HasKey(ps => new { ps.SongId, ps.PerformerId });
+            });
         }
     }
 }
--- LINQ/MusicHub Database/MusicHub/StartUp.cs
+++ LINQ/MusicHub Database/MusicHub/StartUp.cs
@@ -1,30 +1,111 @@
 ﻿namespace MusicHub
 {
     using System;
-
+    using System.Globalization;
+    using System.Linq;
+    using System.Text;
     using Data;
     using Initializer;
+
 
     public class StartUp
     {
         public static void Main(string[] args)
         {
-            MusicHubDbContext context = 
+            MusicHubDbContext context =
                 new MusicHubDbContext();
 
             DbInitializer.ResetDatabase(context);
 
             //Test your solutions here
+            // Console.WriteLine(ExportAlbumsInfo(context, 9));
+
+            Console.WriteLine(ExportSongsAboveDuration(context, 4));
         }
 
         public static string ExportAlbumsInfo(MusicHubDbContext context, int producerId)
         {
-            throw new NotImplementedException();
+            var info = context.Albums.ToArray()
+                      .Where(a => a.ProducerId == producerId)
+                      .OrderByDescending(a => a.Price)
+                      .Select(a => new
+                      {
+                          AlbumName = a.Name,
+                          ReleaseDate = a.ReleaseDate.ToString("MM/dd/yyyy", CultureInfo.InvariantCulture),
+                          ProducerName = a.Producer.Name,
+                          TotalPrice = a.Price.ToString("f2"),
+                          Songs = a.Songs.ToArray().Select(s => new
+                          {
+                              SongName = s.Name,
+                              Price = s.Price.ToString("f2"),
+                              Writer = s.Writer.Name
+                          })
+                          .OrderByDescending(s => s.SongName)
+                          .ThenBy(s => s.Writer)
+                          .ToArray()
+                      })
+                      .ToArray();
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var album in info)
+            {
+                sb.AppendLine($"-AlbumName: {album.AlbumName}")
+                    .AppendLine($"-ReleaseDate: {album.ReleaseDate}")
+                    .AppendLine($"-ProducerName: {album.ProducerName}")
+                    .AppendLine("-Songs:");
+
+                int i = 1;
+
+                foreach (var song in album.Songs)
+                {
+                    sb.AppendLine($"---#{i++}")
+                        .AppendLine($"---SongName: {song.SongName}")
+                        .AppendLine($"---Price: {song.Price}")
+                        .AppendLine($"---Writer: {song.Writer}");
+                }
+
+
+                sb.AppendLine($"-AlbumPrice: {album.TotalPrice}");
+            }
+            return sb.ToString().TrimEnd();
         }
 
         public static string ExportSongsAboveDuration(MusicHubDbContext context, int duration)
         {
-            throw new NotImplementedException();
+            var info = context.Songs
+                .ToArray()
+                .Where(s => s.Duration.TotalSeconds >duration)
+                .Select(s => new
+                {
+                    SongName = s.Name,
+                    Performer = s.SongPerformers.ToArray().
+                    Select(p => $"{p.Performer.FirstName} {p.Performer.LastName}")
+                    .FirstOrDefault(),
+                    Writer = s.Writer.Name,
+                    Producer = s.Album.Producer.Name,
+                    Duration = s.Duration.ToString("c")
+
+                })
+                .OrderBy(s => s.SongName)
+                .ThenBy(s => s.Writer)
+                .ThenBy(s => s.Performer)
+                .ToArray();
+
+            StringBuilder sb = new StringBuilder();
+
+            int i = 1;
+            foreach (var song in info)
+            {
+                sb.AppendLine($"-Song #{i++}")
+                   .AppendLine($"---SongName: {song.SongName}")
+                   .AppendLine($"---Writer: {song.Writer}")
+                   .AppendLine($"---Performer: {song.Performer}")
+                   .AppendLine($"---AlbumProducer: {song.Producer}")
+                   .AppendLine($"---Duration: {song.Duration}");
+            }
+
+            return sb.ToString().TrimEnd();
         }
     }
 }
